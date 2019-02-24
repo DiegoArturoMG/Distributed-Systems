43c6acd3bc0dca3bef7c79f37e578e4600fb7011
Diego Arturo Morales González <diegoarturo2121@gmail.com>
2019-02-24 12:26:31 +0800

    Modify assignment 6
    Modified Assignment 6 moodle

--- Practica 6/Moodle/moodle.cpp
+++ Practica 6/Moodle/moodle.cpp
@@ -1,44 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-  int num, i, inicia, j, k, l;
-
-  cout << "Ingresa un numero natural: " << endl;
-  cin >> num;
-  bool vector[num];
-
-  cout << endl;
-
-  for (size_t i = 1; i < num; i++){
-    /* code */
-    vector[i] = {true};
-  }
-
-  for (size_t inicia = 2; inicia <= num; inicia++) {
-    /* code */
-    j = 2 * inicia;
-    k = 3 * inicia;
-    if (j <= num && j%2 == 0) {
-      /* code */
-      vector[j] = {false};
-    }
-    if (k <= num && k%2 == 1) {
-      /* code */
-      vector[k] = {false};
-    }
-  }
-
-  for (size_t l = 2; l <= num; l++) {
-    /* code */
-    if (vector[l] == true) {
-      /* code */
+void imprimir(bool vec[],int num){
+  cout << "\nLos numeros primos son: " << endl;
+  for(int l=2;l<=num;l++){
+    if(vec[l] == true){
       cout << l << endl;
     }
   }
+}
 
-  cout << endl;
+void calcularPrimos(bool* vec, int num){
+  int j=0,k=0;
+
+  for(int ini=2;ini<=num;ini++){
+    j = 2 * ini;
+    k = 3 * ini;
+    if(j<=num && j%2==0){
+      vec[j] = false;
+    }
+    if(k<=num && k%2==1) {
+      vec[k] = false;
+    }
+  }
+}
+
+void iniciarVect(bool* vec,int num){
+  for(int i=1;i<num;i++){
+    vec[i] = true;
+  }
+}
+
+int main(){
+  int num,i,ini,l;
+
+  cout << "Ingresa un numero: " << endl;
+  cin >> num;
+  
+  bool vec[num];
+
+  iniciarVect(&vec[0],num);
+  calcularPrimos(&vec[0],num);
+  imprimir(vec,num);
 
   return 0;
 }
