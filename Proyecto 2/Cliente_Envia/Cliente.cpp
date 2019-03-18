7f7e111b465817f06f3866d540746a052861a190
Diego Arturo Morales Gonzalez <diegoarturo2121@gmail.com>
2019-03-18 09:41:00 -0600

    Proyect 2

--- Proyecto 2/Cliente_Envia/Cliente.cpp
+++ Proyecto 2/Cliente_Envia/Cliente.cpp
@@ -3,27 +3,30 @@
 using namespace std;
 
 #define puerto 7300
+#define puerto2 7400
 #define MAX_HILOS 2
-
-const char *argumento;
 
 PaqueteDatagrama enviarDat(char* valor){
     return PaqueteDatagrama(valor, sizeof(int),"127.0.0.1", puerto);
 }
 
-void * enviar(void * ch[]){
+void * enviar(void * arg){
 
-    printf("Hilo: %u\n",pthread_self());
+    int *argumento = (int *) arg;
 
     SocketDatagrama socket = SocketDatagrama(puerto);
     int numeros[1],val=1;
     numeros[0] = val;
     //Convertimos de char* a int
-    long n = strtol(argumento, NULL, 10);
+    long n = (long int)*argumento;
+
+    //cout << n << endl;
 
     do{
+        //cout << "Se envia paquete" << endl;
         PaqueteDatagrama datagramaEnvia = enviarDat((char*)numeros);
         socket.envia(datagramaEnvia);
+        //cout << "Se ha enviado paquete" << endl;
         val++;
         numeros[0] = val;
 
@@ -33,19 +36,67 @@ void * enviar(void * ch[]){
             num[0] = 0;
             PaqueteDatagrama datagramaEnvia = enviarDat((char*) num);
             socket.envia(datagramaEnvia);
+            //cout << "Se han enviado todos los paquetes" << endl;
         }
 
     }while(numeros[0] <= n);
 
-
-    sleep(3);
-    pthread_exit(0);
+    //sleep(3);
+    //pthread_exit(0);
 
 }
 
+void imprimirPaquetesPerdidos(int * paquetesP, int cant, long n){
+    cout << "Pauqetes perdidos: " << cant << endl;
+    for(int i=0;i<cant;i++){
+        cout << *paquetesP++ << endl;
+    }
+    double result = (100.00 - ((cant*100.00)/n));
+    cout << "Porcentaje de paquetes enviados: " << result << endl;
+}
+
+void * recibir(void * arg){
+
+    int *argumento = (int *) arg;
+    long n = (long int)*argumento;
+
+    SocketDatagrama socket = SocketDatagrama(puerto2);
+    int * paquetes_perdidos = NULL;
+    int cant = 2,cant_aux=0;
+
+    paquetes_perdidos = (int *)malloc(1*sizeof(int));
+
+    cout << "Esperando respuesta de Servidor..." << endl;
+
+    while(1){
+
+        PaqueteDatagrama datagramaRecibe =  PaqueteDatagrama(sizeof(int));
+        socket.recibe(datagramaRecibe);
+
+        paquetes_perdidos = (int *)realloc(paquetes_perdidos,cant*sizeof(int));
+
+        int * respuesta = (int*)datagramaRecibe.obtieneDatos();
+
+        //cout << "Llego paquete perdido: " << *respuesta << endl;
+        if(*respuesta == 0){
+            //cout << "Se ha terminado envio de paquetes" << endl;
+            imprimirPaquetesPerdidos(&paquetes_perdidos[0],cant_aux,n);
+            exit(1);
+        }else{
+            paquetes_perdidos[cant_aux] = *respuesta;
+            cant_aux++;cant++;
+        }
+
+    }
+
+    //sleep(3);
+    //pthread_exit(0);
+}
+
 int main(int argc, char const *argv[]){
-    
-    argumento = argv[1];
+
+    //Se le envia parametro por consola
+    long conv = atoi(argv[1]);
 
     int i;
     pthread_attr_t atributos;
@@ -54,7 +105,10 @@ int main(int argc, char const *argv[]){
     pthread_attr_init(&atributos);
     pthread_attr_setdetachstate(&atributos,PTHREAD_CREATE_DETACHED);
 
-    pthread_create(&thid[0],&atributos,enviar,*argv[1]);
+    pthread_create(&thid[0],&atributos,enviar,&conv);
+    pthread_create(&thid[1],&atributos,recibir,&conv);
 
-    sleep(6);    
+    sleep(1000); 
+
+    return 0;   
 }
--- Proyecto 2/Cliente_Recibe/Cliente.cpp
+++ Proyecto 2/Cliente_Recibe/Cliente.cpp
@@ -3,41 +3,67 @@
 using namespace std;
 
 #define puerto 7300
+#define puerto2 7400
+#define MAX_HILOS 2
 
-PaqueteDatagrama enviar(char* valor){
-    return PaqueteDatagrama(valor, sizeof(int),"127.0.0.1", puerto);
+PaqueteDatagrama enviarDat(char* valor){
+    return PaqueteDatagrama(valor, sizeof(int),"127.0.0.1", puerto2);
 }
 
-int main(int argc, char const *argv[]){
-	SocketDatagrama socket = SocketDatagrama(puerto);
+void * recibir(void *){
+
+    SocketDatagrama socket = SocketDatagrama(puerto);
         
     int identificador=1;
+    int val[1];
 
     cout << "Esperando clientes..." << endl;
 
     while(1){
-
+        //cout << "Se espera paquete" << endl;
         PaqueteDatagrama datagramaRecibe =  PaqueteDatagrama(sizeof(int));
         socket.recibe(datagramaRecibe);
-        int * ans = (int*)datagramaRecibe.obtieneDatos();
+        //cout << "Se recibe paquete" << endl;
+        int * respuesta = (int*)datagramaRecibe.obtieneDatos();
 
-        if(identificador == *ans){
-            cout << "Se ha recibido correctamente: " << *ans << endl;
+        if(identificador == *respuesta){
+            //cout << "Se ha recibido correctamente: " << *respuesta << endl;
             identificador++;
-        }else if(*ans == 0){
-            cout << "Se recibieron todos los paquetes correctamente!" << endl;
+        }else if(*respuesta == 0){
+            //cout << "Se ha recibido paquete 0" << endl;
+            val[0] = 0;
+            PaqueteDatagrama datagramaEnvia = enviarDat((char*)val);
+            socket.envia(datagramaEnvia);
+            //cout << "Se ha regresado paquete 0" << endl;
             identificador = 1;
         }else{
-            cout << "Se esperaba: " << identificador << endl;
-            cout << "Se recibio: " << *ans << endl;
-            PaqueteDatagrama datagramaEnvia = enviar((char*)ans);
+            //cout << "Se esperaba: " << identificador << endl;
+            //cout << "Se recibio: " << *respuesta << endl;
+            val[0] = identificador;
+            identificador++;
+            PaqueteDatagrama datagramaEnvia = enviarDat((char*)val);
             socket.envia(datagramaEnvia);
-            exit();
+            //cout << "Se ha regresado paquete fallido" << endl;
         }
 
-
-
     }
+
+    //sleep(3);
+    //pthread_exit(0);
+}
+
+int main(int argc, char const *argv[]){
+
+    int i;
+    pthread_attr_t atributos;
+    pthread_t thid[MAX_HILOS];
+
+    pthread_attr_init(&atributos);
+    pthread_attr_setdetachstate(&atributos,PTHREAD_CREATE_DETACHED);
+
+    pthread_create(&thid[0],&atributos,recibir,NULL);
+
+    sleep(10000);    
 
     return 0;
 }
